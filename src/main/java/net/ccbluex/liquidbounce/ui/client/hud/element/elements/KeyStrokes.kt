238839e83bfedd6cda8ab2e9acd470e2321c9b9c
jwaxy <me@yxawj.aleeas.com>
2023-01-19 11:49:56 +0300

    juul

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/KeyStrokes.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/KeyStrokes.kt
@@ -42,7 +42,7 @@ class KeyStrokes : Element(5.0, 25.0, 1.5F, Side.default()) {
     private val outlineBoldValue = IntegerValue("OutlineBold", 1, 0, 5)
     private val outlineRainbow = BoolValue("OutLineRainbow", false)
     private val fontValue = FontValue("Font", Fonts.font35)
-    val keyStyleValue = ListValue("Mode", arrayOf("Custom", "Jello", "Juul"), "Jello")
+    val keyStyleValue = ListValue("Mode", arrayOf("Custom", "Jello", "Juul"), "Juul")
 
     init {
         keys.add(KeyStroke(mc.gameSettings.keyBindForward, 16, 0, 15, 15).initKeyName())
